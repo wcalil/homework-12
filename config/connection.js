d76e85e6ac7959b587f764bc8f5f064ca169fd51
Wilson Calil <wilson.calil@outlook.com>
2020-04-18 23:49:46 -0500

    Fixing connection.js to connect with JAWSDB.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  // port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+};
 
+connection.connect();
 exports.connection = connection;
